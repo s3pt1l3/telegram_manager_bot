2222eb78f4e1e81f9331b0a2c9e1d991c7c76455
TriangulumGalaxy <59769865+TriangulumGalaxy@users.noreply.github.com>
2021-09-11 22:48:23 +0300

    feat: add registration of filters

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-from loader import dp
-
-
 async def on_startup(dp):
-    pass
+    from apps import _filters
+    from loader import dp
+
+    dp.bind_filter(*_filters)
 
 if __name__ == '__main__':
     from aiogram import executor
+    from apps import dp
 
     executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
