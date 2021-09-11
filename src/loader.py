d9a30cf2a438c08989df8b61bded53b19f660766
s3pt1l3 <powerline.studio@gmail.com>
2021-09-11 22:21:22 +0300

    ref: changed token import and added db import

--- src/loader.py
+++ src/loader.py
@@ -1,7 +1,8 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from settings import TOKEN
+from modules.config.bot_config import TOKEN
+from modules.database.database import db
 
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
