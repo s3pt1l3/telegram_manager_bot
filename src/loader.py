fc01c190f7cf0e4014bb2bda5cdd20caab056f31
TriangulumGalaxy <59769865+TriangulumGalaxy@users.noreply.github.com>
2021-09-11 22:22:05 +0300

    fix: update token import

--- src/loader.py
+++ src/loader.py
@@ -1,11 +1,10 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from modules.config.bot_config import TOKEN
 
-from settings import TOKEN
 
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 __all__ = ['bot', 'storage', 'dp', 'db']
-
