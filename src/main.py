0c6669b83f7d3edf426179c8b9efd7e8ce30f3bc
s3pt1l3 <powerline.studio@gmail.com>
2021-09-11 22:22:00 +0300

    feat: added db connection

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 from loader import dp
 
+async def flush_all():
+    from loader import db
+    
+    print('Очистка базы...')
+    await db.gino.drop_all()
+    print('Готово')
+    print('Создание таблиц...')
+    await db.gino.create_all()
+    print('Готово')
 
 async def on_startup(dp):
-    pass
+    from modules.database import database
+    
+    print('Подключение к базе данных...')
+    await database.on_startup(dp)
+    print('Подключение установлено')
+    await flush_all()
 
 if __name__ == '__main__':
     from aiogram import executor
