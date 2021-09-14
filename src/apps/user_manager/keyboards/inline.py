5c7dd6b2c494ba950023acb11b02c8d40b44ad22
TriangulumGalaxy <59769865+TriangulumGalaxy@users.noreply.github.com>
2021-09-14 23:49:21 +0300

    feat: update the functionality of user_manager app
    new:
    adding interfaces:
      - calendar tasks
      - daily task
      - non-deadline tasks

--- src/apps/user_manager/keyboards/inline.py
+++ src/apps/user_manager/keyboards/inline.py
@@ -2,4 +2,5 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 task_type_keyboard = InlineKeyboardMarkup(row_width=2)
 task_type_keyboard.add(InlineKeyboardButton('Ежедневная', callback_data='task:routine'),
-                       InlineKeyboardButton('Без дедлайна', callback_data='task:nodeadline'))
+                       InlineKeyboardButton('Без дедлайна', callback_data='task:nodeadline'),
+                       InlineKeyboardButton('Календарная', callback_data='task:calendar'))
