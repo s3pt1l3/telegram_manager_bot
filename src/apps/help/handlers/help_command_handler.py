ea9311d0426d9f1f8db23085ecfb35c9f9ad6692
TriangulumGalaxy <59769865+TriangulumGalaxy@users.noreply.github.com>
2021-09-14 21:18:01 +0300

    feat: add test help command that adds new user to db

--- src/apps/help/handlers/help_command_handler.py
+++ src/apps/help/handlers/help_command_handler.py
@@ -1,8 +1,10 @@
 from aiogram.types import Message
 from aiogram.dispatcher import FSMContext
 from loader import dp
+from modules.database.schemas import user
 
 
 @dp.message_handler(commands=['help'], state='*')
 async def handle_del_command(message: Message, state: FSMContext):
     await message.answer('Help message')
+    await user.add(message.from_user.id, message.from_user.username)
