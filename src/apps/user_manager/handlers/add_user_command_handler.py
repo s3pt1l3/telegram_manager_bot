44f3f1f0a16cc074432bb5e222239e20a5e6874e
TriangulumGalaxy <59769865+TriangulumGalaxy@users.noreply.github.com>
2021-09-16 20:45:44 +0300

    feat: add new commnad add admin

--- src/apps/user_manager/handlers/add_user_command_handler.py
+++ src/apps/user_manager/handlers/add_user_command_handler.py
@@ -14,4 +14,16 @@ async def handle_add_command(message: Message, state: FSMContext):
         return
 
     usr = await user.select_by_tag(args[0])
-    await user.update(usr.user_id, args[0], usr.is_admin)
+    await user.update(usr.user_id, args[0], usr.is_admin, True)
+
+
+@dp.message_handler(IsAdmin(), commands=['addadmin'], state='*')
+async def handle_newadmin(message: Message, state: FSMContext):
+    args = message.get_args()
+
+    if not args:
+        await message.answer('Использование: /addadmin [Тег пользователя]')
+        return
+
+    usr = await user.select_by_tag(args[0])
+    await user.update(usr.user_id, args[0], True, True)
