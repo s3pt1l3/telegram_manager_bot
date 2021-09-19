6abdc2ff75c159ef554462d1feaca287913f91b4
TriangulumGalaxy <59769865+TriangulumGalaxy@users.noreply.github.com>
2021-09-19 22:01:44 +0300

    feat: add cancel command and cancel callback handler

--- src/apps/help/handlers/help_command_handler.py
+++ src/apps/help/handlers/help_command_handler.py
@@ -1,8 +1,21 @@
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
 from aiogram.dispatcher import FSMContext
 from apps.user_manager.filters import IsAdmin
 from loader import dp
 from modules.database.schemas import user
+
+
+@dp.message_handler(commands=['cancel'], state='*')
+async def cancel(message: Message, state: FSMContext):
+    await state.finish()
+    await message.answer('Отменено', reply_markup=ReplyKeyboardRemove())
+
+
+@dp.callback_query_handler(text='cancel', state='*')
+async def cancel_callback(call: CallbackQuery, state: FSMContext):
+    await state.finish()
+    await call.answer('Отменено')
+    await call.message.delete()
 
 
 @dp.message_handler(commands=['start'])
