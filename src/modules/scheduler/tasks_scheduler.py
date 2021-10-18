a0b8cfdb1b6a67b5badfe52be61af9de1a9a7e1e
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-10-18 15:29:09 +0300

    Feat(notifications): changed shedule

--- src/modules/scheduler/tasks_scheduler.py
+++ src/modules/scheduler/tasks_scheduler.py
@@ -2,11 +2,14 @@ import asyncio
 import aioschedule
 from apps.task_notifications.daily.daily_notifications import send_notifications as daily_notifications
 from apps.task_notifications.periodic.periodic_notifications import send_notifications as periodic_notifications
+from apps.task_notifications.weekly.weekly_notifications import send_notifications as weekly_notifications
 
 
 async def scheduler():
-    aioschedule.every(15).days.do(periodic_notifications)
+    aioschedule.every(10).days.at("11:00").do(periodic_notifications)
+    aioschedule.every(10).days.at("21:00").do(periodic_notifications)
     aioschedule.every().day.at("11:00").do(daily_notifications)
+    aioschedule.every().week.at("11:00").do(weekly_notifications)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
