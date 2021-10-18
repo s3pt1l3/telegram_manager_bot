1a1622f1ecf401a374398ceffd861a7175f3303a
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-10-18 15:40:12 +0300

    Feat(keyboard): added weekly task option

--- src/apps/user_manager/keyboards/inline.py
+++ src/apps/user_manager/keyboards/inline.py
@@ -1,7 +1,11 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, callback_query
 
 task_type_keyboard = InlineKeyboardMarkup(row_width=2)
 task_type_keyboard.add(InlineKeyboardButton('Ежедневная', callback_data='task:routine'),
-                       InlineKeyboardButton('Без дедлайна', callback_data='task:nodeadline'),
-                       InlineKeyboardButton('Календарная', callback_data='task:calendar'),
+                       InlineKeyboardButton(
+                           'Без дедлайна', callback_data='task:nodeadline'),
+                       InlineKeyboardButton(
+                           'Календарная', callback_data='task:calendar'),
+                       InlineKeyboardButton(
+                           'Еженедельная', callback_data='tasl:weekly'),
                        InlineKeyboardButton('Отмена', callback_data='cancel'))
