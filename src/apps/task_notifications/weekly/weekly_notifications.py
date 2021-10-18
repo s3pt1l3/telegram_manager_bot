65c7a462940796d8f87f317d883b5f197f3cb5eb
TriangulumGalaxy <59769865+TriangulumGalaxy@users.noreply.github.com>
2021-10-18 22:09:44 +0300

    feat: add notifications for admins

--- src/apps/task_notifications/weekly/weekly_notifications.py
+++ src/apps/task_notifications/weekly/weekly_notifications.py
@@ -4,6 +4,8 @@ from modules.database.schemas import user, weekly_task
 
 async def send_notifications():
     employes = await user.select_all_employes()
+    admins = await user.select_all()
+
     for employee in employes:
         tasks = await weekly_task.select_by_user(employee.user_id)
         count = 1
@@ -15,3 +17,5 @@ async def send_notifications():
                 count += 1
 
             await bot.send_message(employee.user_id, mes)
+            for admin in admins:
+                await bot.send_message(admin.user_id, f"Задачи пользователя: {employee.user_id}\n" + mes[mes.find('Ваши задачи на сегодня:'):])
